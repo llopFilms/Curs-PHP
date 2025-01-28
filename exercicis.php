a805c902249870c38fb131cf685384681ac75457
llopFilms <llopfilms@gmail.com>
2025-01-28 22:45:49 +0100

    Actualització

--- exercicis.php
+++ exercicis.php
@@ -3,33 +3,69 @@
 echo '<h1>Exercicis</h1>';
 echo '<p>Exercici 1</p>';
 
-$nom = 'Ramon';
-$edat = 25;
+$nom       = 'Ramon';
+$edat      = 25;
 $professio = 'Desenvolupador';
 
 echo 'El meu nom és ' . $nom . ' i tinc ' . $edat . ' anys. La meva feina és ' . $professio . '.' . '<br>';
 
 echo '<p>Exercici 2</p>';
 
-$preu = 320;
-$iva = 21;
-$preuFinal = $preu + ($preu * $iva / 100);
+$preu      = 320;
+$iva       = 21;
+$preuFinal = $preu + ( $preu * $iva / 100 );
 
 echo 'El preu sense IVA es de ' . $preu . '€ .' . '<br>';
 echo 'El preu amb IVA es de ' . $preuFinal . '€.' . '<br>';
 
 echo '<p>Exercici 3</p>';
 
-$nom2 = 'Ramon';
-$edat2 = 35;
+$nom2        = 'Ramon';
+$edat2       = 35;
 $edatConduir = 18;
 
 echo 'El meu nom es ' . $nom2 . ' i tinc ' . $edat2 . ' anys.' . '<br>';
 echo 'Per tant, si la meva edat és major o igual que ' . $edatConduir . ' anys, puc conduir.' . '<br>';
-echo 'És la meva edat menor que ' . $edatConduir . ' anys? ' . (int)($edat2 < $edatConduir) . ' Si és 0, puc conduir.' . '<br>';
-echo ('És la meva edat igual que ' . $edatConduir . ' anys? ' . (int)($edat2 == $edatConduir) . ' Si es 0, no sabem si tinc més o menys de ' . $edatConduir . ' anys. Però segur que no tinc ' . $edatConduir . ' anys.' . '<br>');
+echo 'És la meva edat menor que ' . $edatConduir . ' anys? ' . (int) ( $edat2 < $edatConduir ) . ' Si és 0, puc conduir.' . '<br>';
+echo( 'És la meva edat igual que ' . $edatConduir . ' anys? ' . (int) ( $edat2 == $edatConduir ) . ' Si es 0, no sabem si tinc més o menys de ' . $edatConduir . ' anys. Però segur que no tinc ' . $edatConduir . ' anys.' . '<br>' );
 
 echo '<p>Exercici 4</p>';
 
-echo 'És la meva edat major de ' . $edatConduir . ' anys? ' . !(int)($edat2 < $edatConduir) . ' Si és 1, puc conduir.' . '<br>';
+echo 'És la meva edat major de ' . $edatConduir . ' anys? ' . !(int) ( $edat2 < $edatConduir ) . ' Si és 1, puc conduir.' . '<br>';
 
+echo '<p>Exercici 5</p>';
+
+$nomEstudiant  = 'Pere';
+$edatEstudiant = 17;
+define( 'DATA_DE_NAIXEMENT', 2008 );
+
+echo 'El meu nom es ' . $nomEstudiant . ' i tinc ' . $edatEstudiant . ' anys. Vaig néixer el ' . DATA_DE_NAIXEMENT . '.' . '<br>';
+
+echo '<p>Exercici 6</p>';
+
+$marquesDeCotxe = ['Opel', 'Seat', 'Ebro', 'Iveco'];
+echo 'Les marques de cotxe que tenim son: ' . implode( ', ', $marquesDeCotxe ) . '.';
+echo '<br>';
+echo 'Mostrem per pantalla la posició 2: ' . $marquesDeCotxe[2] . '.';
+echo '<br>';
+echo 'Canviem el valor de la posició 2 a "Ford".';$marquesDeCotxe[2] = 'Ford';
+echo '<br>';
+echo 'Així queda l\'array: ' . implode( ', ', $marquesDeCotxe ) . '.';
+echo '<br>';
+echo "Mida de l'array de cotxes: " . count( $marquesDeCotxe ) . '.';
+echo '<br>';
+echo 'Elimina els primer valor de l\'array amb unset()';
+unset( $marquesDeCotxe[0] );
+echo '<br>';
+echo 'Així queda l\'array: ' . implode( ', ', $marquesDeCotxe ) . '.';
+echo '<br>';
+echo 'I així queden els elements: ';
+var_dump( $marquesDeCotxe );
+echo '<br>';
+echo 'Afegim \'Fiat\' al final sense especificar la posició amb nomDelArray[] = valor';
+$marquesDeCotxe[] = 'Fiat';
+echo '<br>';
+echo 'Així queda l\'array: ' . implode( ', ', $marquesDeCotxe ) . '.';
+echo '<br>';
+echo 'I així queden els elements: ';
+var_dump( $marquesDeCotxe );
--- sintaxi-i-variables.php
+++ sintaxi-i-variables.php
@@ -1,5 +1,5 @@
 <?php
-echo '<h1>Tipus de dades</h1>';
+echo '<h2>Tipus de dades</h2>';
 
 $num1 = 10;
 $num2 = -5;
@@ -26,7 +26,7 @@ var_dump( $colors );
 $noms = array( 'nom1' => 'Maria', 'nom2' => 'Mercè', 'nom3' => 'Glòria' );
 var_dump( $noms );
 
-echo '<h1>Operadors</h1>';
+echo '<h2>Operadors</h2>';
 
 $num3 = 20;
 $num4 = 25;
@@ -44,32 +44,146 @@ echo '<br>';
 echo 'Potència: ' . $num3 ** $num4;
 echo '<br>';
 
-echo '<h1>Comparadors</h1>';
+echo '<h2>Comparadors</h2>';
 
-echo 'Major que: ' . (int)( $num3 > $num4 );
+echo 'Major que: ' . (int) ( $num3 > $num4 );
 echo '<br>';
-echo 'Menor que: ' . (int)( $num3 < $num4 );
+echo 'Menor que: ' . (int) ( $num3 < $num4 );
 echo '<br>';
-echo 'Major o igual que: ' . (int)( $num3 >= $num4 );
+echo 'Major o igual que: ' . (int) ( $num3 >= $num4 );
 echo '<br>';
-echo 'Menor o igual que: ' . (int)( $num3 <= $num4 );
+echo 'Menor o igual que: ' . (int) ( $num3 <= $num4 );
 echo '<br>';
-echo 'Diferent: ' . (int)( $num3 != $num4 );
+echo 'Diferent: ' . (int) ( $num3 != $num4 );
 echo '<br>';
-echo 'Diferent exacte: ' . (int)( $num3 !== $num4 );
+echo 'Diferent exacte: ' . (int) ( $num3 !== $num4 );
 echo '<br>';
-echo 'Igual: ' . (int)( $num3 == $num4 );
+echo 'Igual: ' . (int) ( $num3 == $num4 );
 echo '<br>';
-echo 'Igual exacte: ' . (int)( $num3 === $num4 );
+echo 'Igual exacte: ' . (int) ( $num3 === $num4 );
 echo '<br>';
 
-echo '<h1>Lògics</h1>';
+echo '<h2>Lògics</h2>';
 
-echo 'OR: ' . (int)( $num3 > $num4 || $num3 < $num4 );
+echo 'OR: ' . (int) ( $num3 > $num4 || $num3 < $num4 );
 echo '<br>';
-echo 'AND: ' . (int)( $num3 > $num4 && $num3 < $num4 );
+echo 'AND: ' . (int) ( $num3 > $num4 && $num3 < $num4 );
 echo '<br>';
-echo 'NOT: ' . (int)( ! ( $num3 > $num4 ) );
+echo 'NOT: ' . (int) ( !( $num3 > $num4 ) );
 echo '<br>';
-echo 'NOT: ' . (int)( ! ( $num3 < $num4 ) );
+echo 'NOT: ' . (int) ( !( $num3 < $num4 ) );
 echo '<br>';
+
+echo '<h2>Variables i constants</h2>';
+
+$nombre_pomes = 10;
+echo 'La variable $nombre_pomes té una valor de ' . $nombre_pomes;
+echo '<br>';
+
+$num = 58;
+echo 'La variable $num val ' . $num;
+echo '<br>';
+
+echo '<h2>Constants</h2>';
+
+define( 'NOM', 'Ramon' );
+echo 'La constant NOM, s\'escriu en majúsules, es defineix amb define ( \'NOM\', \'Ramon\' ) i val ' . NOM . '. <br>';
+const NOM2 = 'Ramon';
+echo 'O sinó es pot definir directament com const NOM2 = \'Ramon\' i val ' . NOM . '. <br>';
+echo '<br>';
+define( 'PI', 3.14 );
+echo 'La constant PI, s\'escriu en majúsules, es defineix amb define ( \'PI\', 3.14 )\' i val ' . PI . '.<br>';
+const PI2 = 3.14;
+echo 'O sinó es pot definir directament com const PI2 = 3.14 i val ' . PI . '.<br>';
+
+echo '<h2>Arrays</h2>';
+
+$llenguatges = ['PHP', 'HTML', 'CSS', 'JS'];
+echo 'Llenguatges: ' . $llenguatges[0] . ', ' . $llenguatges[1] . ', ' . $llenguatges[2] . ', ' . $llenguatges[3] . '.';
+echo '<br>';
+echo 'Es pot passar array a cadena amb implode() ';
+echo 'Llenguatges: ' . implode( ', ', $llenguatges ) . '.';
+echo '<br>';
+echo 'Per veure index (en array simple) i elements associats, podem fer servir var_dump()';
+echo '<br>';
+var_dump( $llenguatges );
+
+echo '<br><br>';
+
+$flors = array(
+    'florVermella' => 'rosa',
+    'florBlanca'   => 'margarida',
+    'florGroga'    => 'girasol',
+    'florVioleta'  => 'violeta',
+);
+echo 'Flors: ' . $flors['florVermella'] . ', ' . $flors['florBlanca'] . ', ' . $flors['florGroga'] . ', ' . $flors['florVioleta'] . '.';
+echo '<br>';
+echo 'Es pot passar array a cadena amb implode() ';
+echo 'Flors: ' . implode( ', ', array_values( $flors ) ) . '.';
+
+echo '<br><br>';
+
+echo 'Per comptar els elements d\'un array, fem servir count()';
+echo '<br>';
+echo 'Elemenst a l\'array Llenguatges: ' . count( $llenguatges );
+echo '<br>';
+echo 'Elements a l\'array Flors: ' . count( $flors );
+
+var_dump( $flors );
+
+echo '<br><br>';
+
+echo '<br>Per veure un element de l\'array podem fer servir echo cridant index o valor associat';
+echo '<br>';
+echo '$llenguatges[2] --> ' . $llenguatges[2] . '<br>';
+echo '$flors[\'florVermella\'] --> ' . $flors['florVermella'];
+
+echo '<br><br>';
+
+echo 'Per canviar un valor d\'un array podem fer servir índex conegut o valor associat conegut';
+echo '<br>';
+$llenguatges[2] = 'C#';
+echo '$llenguatges[2] --> ' . $llenguatges[2] . '<br>';
+$flors['florVermella'] = 'tulipa';
+echo '$flors[\'florVermella\'] --> ' . $flors['florVermella'];
+
+echo '<br><br>';
+
+echo 'Per afegir un element al final d\'un array podem fer servir array_push(). Afegim l\'element \'C++\' al final.';
+echo '<br>';
+array_push( $llenguatges, 'C++' );
+var_dump( $llenguatges );
+
+echo '<br><br>';
+
+echo 'També podem assignar un valor sense especificar l\'índex amb $nomArray[] = valor';
+$llenguatges[] = 'Java';
+var_dump( $llenguatges );
+
+echo '<br><br>';
+
+echo 'Per afegir un element al principi d\'un array podem fer servir array_unshift(). Afegim l\'element \'C\' al principi.';
+echo '<br>';
+array_unshift( $llenguatges, 'C' );
+var_dump( $llenguatges );
+
+echo '<br><br>';
+
+echo 'Per eliminar l\'ultim element d\'un array podem fer servir array_pop(). Eliminació de l\'element \'Java\' al final.';
+echo '<br>';
+array_pop( $llenguatges );
+var_dump( $llenguatges );
+
+echo '<br><br>';
+
+echo 'Per eliminar el primer element d\'un array podem fer servir array_shift(), eliminacio de l\'element \'C\' al principi.';
+echo '<br>';
+array_shift( $llenguatges );
+var_dump( $llenguatges );
+
+echo '<br><br>';
+
+echo 'Per eliminar un element d\'una posició coneguda de l\'array podem fer servir unset(), eliminacio de l\'element \'C#\' a index 2.';
+echo '<br>';
+unset( $llenguatges[2] );
+var_dump( $llenguatges );
